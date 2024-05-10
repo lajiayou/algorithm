8c58fd2c2a38b9a3463e4a0aaade579e875e469b
lijiayang <389184375@qq.com>
2024-05-10 21:59:59 +0800

    2、二分查找

--- array/binarySearch/main.cpp
+++ array/binarySearch/main.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
-int main() {
+int binarySearch(int num[],int count, int target){
 
-    
+    int low = 0;
+    int high =count - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int flag = num[mid];
+        if(flag > target){
+            high = mid - 1;
+        }
+        else if (flag < target)
+        {
+            low = mid + 1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int num[7] = {0,1,2,57,78,88,99};
+    int result = binarySearch(num,7, 57);
+    cout << result;
     system("pause");
     return 0;
 }
